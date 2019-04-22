b75f4b16d3770452a4c6c27c4327ba7ca3284c55
balacs13 <bala@pjupyter02.vcse.lab>
2019-04-22 16:08:56 -0500

    modified files to fit in small memory

--- datasets/cats-dogs/run.sh
+++ datasets/cats-dogs/run.sh
@@ -5,5 +5,17 @@ unzip train.zip
 unzip test1.zip
 mkdir -p demo/dogs
 mkdir -p demo/cats
-cp train/cat.7* demo/cats
-cp train/dog.7* demo/dogs
+mv train/cat.7* demo/cats
+mv train/dog.7* demo/dogs
+mkdir -p demo-lite/dogs
+mkdir -p demo-lite/cats
+cp demo/cats/cat.711* demo-lite/cats/.
+cp demo/cats/cat.712* demo-lite/cats/.
+cp demo/cats/cat.713* demo-lite/cats/.
+cp demo/cats/cat.714* demo-lite/cats/.
+cp demo/cats/cat.715* demo-lite/cats/.
+cp demo/dogs/dog.711* demo-lite/dogs/.
+cp demo/dogs/dog.712* demo-lite/dogs/.
+cp demo/dogs/dog.713* demo-lite/dogs/.
+cp demo/dogs/dog.714* demo-lite/dogs/.
+cp demo/dogs/dog.715* demo-lite/dogs/.
